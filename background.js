848c28205ebcf0d9c11288ccd2f5f88b77a0aa3b
nicholasqiao <128782014+nicholasqiao@users.noreply.github.com>
2023-05-31 19:44:34 -0400

    adding css + delete ch logic + auto refresh

--- background.js
+++ background.js
@@ -1,97 +1,160 @@
 // background.js (service worker)
 
-accepted_hosts = [
-  "reaperscans",
-  "asurascans"
-]
+accepted_hosts = ["reaperscans", "asurascans"];
 
 // Add event listener for page navigation
-chrome.webNavigation.onCommitted.addListener(function(details) {
+chrome.webNavigation.onCommitted.addListener(function (details) {
   // Check if the navigation is in the main frame
   if (details.frameId === 0) {
-    coreLogic(details)
+    coreLogic(details);
   }
 });
 
 // Add event listener for active tab changes
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  coreLogic(activeInfo)
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  coreLogic(activeInfo);
 });
 
 coreLogic = (activeInfo) => {
-  chrome.tabs.get(activeInfo.tabId, function(tab) {
-    const currentUrl = tab.url
-    const parts = currentUrl.split("-")
-    const partsByBackslash = currentUrl.split("/")
-    const currentChapter = parts[parts.length - 1]
-    const strippedChapterId = currentChapter.replace(/\/$/, '');
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    const currentUrl = tab.url;
+    const parts = currentUrl.split("-");
+    const partsByBackslash = currentUrl.split("/");
+    const currentChapter = parts[parts.length - 1];
+    const strippedChapterId = currentChapter.replace(/\/$/, "");
     const chapterIdIsInteger = /^\d+$/.test(strippedChapterId);
-    const mainUrl = partsByBackslash[2]
-    let comicName = ""
+    const mainUrl = partsByBackslash[2];
+    let comicName = "";
+    let comicKey = "";
 
-    if (isAcceptedHost(currentUrl, accepted_hosts) && mainUrl.includes("reaperscans") && partsByBackslash.length > 4) {
-      comicName = partsByBackslash[4].replace(/[0-9\s-]/g, '').replace(/chapter/gi, '');
-      updateBookmark(comicName, chapterIdIsInteger, strippedChapterId)
-    } else if (isAcceptedHost(currentUrl, accepted_hosts) && mainUrl.includes("asurascans") && partsByBackslash.length > 4) {
-      comicName = partsByBackslash[3].replace(/[0-9\s-]/g, '').replace(/chapter/gi, '');
-
+    if (
+      isAcceptedHost(currentUrl, accepted_hosts) &&
+      mainUrl.includes("reaperscans") &&
+      partsByBackslash.length > 4
+    ) {
+      comicKey = partsByBackslash[4]
+        .replace(/[0-9\s-]/g, "")
+        .replace(/chapter/gi, "");
+      comicName = partsByBackslash[4]
+        .replace(/[0-9\s-]/g, " ")
+        .replace(/chapter/gi, "")
+        .trim();
+      updateBookmark(
+        comicKey,
+        comicName,
+        chapterIdIsInteger,
+        strippedChapterId,
+        currentUrl
+      );
+    } else if (
+      isAcceptedHost(currentUrl, accepted_hosts) &&
+      mainUrl.includes("asurascans") &&
+      partsByBackslash.length > 4
+    ) {
+      comicKey = partsByBackslash[3]
+        .replace(/[0-9\s-]/g, "")
+        .replace(/chapter/gi, "");
+      comicName = partsByBackslash[3]
+        .replace(/[0-9\s-]/g, " ")
+        .replace(/chapter/gi, "")
+        .trim();
       if (comicName == "manga") {
         // This is the comic base page
-        comicName = partsByBackslash[4].replace(/[0-9\s-]/g, '').replace(/chapter/gi, '');
+        comicName = partsByBackslash[4]
+          .replace(/[0-9\s-]/g, "")
+          .replace(/chapter/gi, "");
       }
-      updateBookmark(comicName, chapterIdIsInteger, strippedChapterId)
+      updateBookmark(
+        comicKey,
+        comicName,
+        chapterIdIsInteger,
+        strippedChapterId,
+        currentUrl
+      );
     } else {
       chrome.action.setBadgeText({
-        text: "???"
+        text: "???",
       });
     }
   });
-}
+};
 
-updateBookmark = (comicName, chapterIdIsInteger, strippedChapterId) => {
-  const key = comicName
+updateBookmark = (
+  comicKey,
+  comicName,
+  chapterIdIsInteger,
+  strippedChapterId,
+  currentUrl
+) => {
+  const key = comicName;
 
   chrome.storage.local.get(key, (result) => {
-    // New comic
     if (result[key] == undefined && chapterIdIsInteger) {
+      // This is for a new comic
       chrome.storage.local.set({
-        [comicName]: strippedChapterId
-      })
-      chrome.action.setBadgeText({
-        text: strippedChapterId
+        [comicName]: [strippedChapterId, currentUrl, comicKey],
       });
-    } else if (/^\d+$/.test(result[key])) {
-      // updating comic
+      chrome.action.setBadgeText({
+        text: strippedChapterId,
+      });
+    } else if (chapterIdIsInteger) {
+      // Updating comic
       if (parseInt(strippedChapterId) > parseInt(result[key])) {
         chrome.storage.local.set({
-          [comicName]: strippedChapterId
-        })
+          [comicName]: [strippedChapterId, currentUrl, comicKey],
+        });
         chrome.action.setBadgeText({
-          text: strippedChapterId
+          text: strippedChapterId,
         });
       } else {
         chrome.action.setBadgeText({
-          text: result[key]
+          text: result[key][0],
         });
       }
-
     }
   });
-}
+};
 
 isAcceptedHost = (currentUrl, hostList) => {
-  return hostList.some(host => {
+  return hostList.some((host) => {
     return currentUrl.includes(host);
   });
 };
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'getStorageData') {
+  if (request.action === "getStorageData") {
     // Retrieve the storage data from chrome.storage.local
     chrome.storage.local.get(null, (storageData) => {
       sendResponse(storageData);
     });
   }
-
   return true; // Indicates that the response will be sent asynchronously
+});
+
+// Listen for messages from the service worker
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "deleteChapter") {
+    const currentUrl = message.data[1];
+
+    const currentComicName = currentUrl
+      .split("/")[4]
+      .replace(/[0-9\s-]/g, " ")
+      .replace(/chapter/gi, "")
+      .trim();
+
+    chrome.storage.local.remove(message.data[0]);
+    if (message.data[0] == currentComicName) {
+      chrome.action.setBadgeText({
+        text: "???",
+      });
+    }
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "clearBadgeText") {
+    chrome.action.setBadgeText({
+      text: "???",
+    });
+  }
 });
--- popup.js
+++ popup.js
@@ -3,36 +3,41 @@
 // DOM elements
 const clearButton = document.getElementById("clear-button");
 const listButton = document.getElementById("list-button");
-const urlDisplay = document.getElementById("url-display");
 const tableBody = document.getElementById("table-body");
 
 // Event listener for login button
 clearButton.addEventListener("click", clear);
-listButton.addEventListener("click", showall);
 
-function clear() {
-  chrome.storage.local.clear(() => {
-    console.log("Storage cleared");
-  });
-}
-
-function showall() {
-  chrome.storage.local.get(null, function (items) {
-    console.log(items);
-  });
-}
-
-// Get the current URL
+let currentUrl;
 chrome.tabs.query(
   {
     active: true,
     currentWindow: true,
   },
   function (tabs) {
-    const currentUrl = tabs[0].url;
-    urlDisplay.textContent = `Current URL: ${currentUrl}`;
+    currentUrl = tabs[0].url;
   }
 );
+
+function clear() {
+  chrome.storage.local.clear();
+  chrome.runtime.sendMessage({
+    action: "clearBadgeText",
+  });
+  location.reload();
+}
+
+function deleteChapter(event) {
+  // Get the value of the data-id attribute
+  const chapterName = event.target.getAttribute("data-id");
+  const currentUrl = event.target.getAttribute("current-url");
+  // Perform other actions based on the buttonId or other data
+  chrome.runtime.sendMessage({
+    action: "deleteChapter",
+    data: [chapterName, currentUrl],
+  });
+  location.reload();
+}
 
 self.addEventListener("message", async (event) => {
   if (event.data.action === "getStorageData") {
@@ -50,15 +55,27 @@ self.addEventListener("message", async (event) => {
     for (const key in storageData) {
       if (storageData.hasOwnProperty(key)) {
         const value = storageData[key];
-
         const row = document.createElement("tr");
         const cell1 = document.createElement("td");
         const cell2 = document.createElement("td");
         const cell3 = document.createElement("td");
 
-        cell1.textContent = key;
-        cell2.textContent = value;
-        cell3.textContent = "action";
+        const link = document.createElement("a");
+        link.href = `${value[1]}`;
+        link.textContent = key;
+        link.target = "_blank";
+        cell1.appendChild(link);
+
+        cell2.textContent = value[0];
+
+        const deleteChapterButton = document.createElement("button");
+        deleteChapterButton.textContent = "Delete";
+        deleteChapterButton.id = "myButton";
+        deleteChapterButton.className = "btn";
+        deleteChapterButton.setAttribute("data-id", key);
+        deleteChapterButton.setAttribute("current-url", currentUrl);
+        deleteChapterButton.addEventListener("click", deleteChapter);
+        cell3.appendChild(deleteChapterButton);
 
         row.appendChild(cell1);
         row.appendChild(cell2);
